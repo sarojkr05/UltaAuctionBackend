509568da247923e22d8b1fa60a288e587befe773
Sanket Singh <singhsanket143@gmail.com>
2024-06-03 19:18:27 +0530

    Added bodyu parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const ServerConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
 
 app.listen(ServerConfig.PORT, async () => {
     await connectDB();
